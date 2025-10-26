cd2038ad784d358a790445c6e0696d5db5d8c3fe
ManupaDev <manupadevsuite@gmail.com>
2025-10-26 20:39:03 +0530

    feat: add validation and limit option for energy generation records retrieval

--- src/application/energy-generation-record.ts
+++ src/application/energy-generation-record.ts
@@ -1,3 +1,5 @@
+import { GetAllEnergyGenerationRecordsQueryDto } from "../domain/dtos/solar-unit";
+import { ValidationError } from "../domain/errors/errors";
 import { EnergyGenerationRecord } from "../infrastructure/entities/EnergyGenerationRecord";
 import { NextFunction, Request, Response } from "express";
 
@@ -8,7 +10,12 @@ export const getAllEnergyGenerationRecordsBySolarUnitId = async (
 ) => {
   try {
     const { id } = req.params;
-    const { groupBy } = req.query;
+    const results = GetAllEnergyGenerationRecordsQueryDto.safeParse(req.query);
+    if (!results.success) {
+      throw new ValidationError(results.error.message);
+    }
+
+    const { groupBy, limit } = results.data;
 
     if (!groupBy) {
       const energyGenerationRecords = await EnergyGenerationRecord.find({
@@ -33,7 +40,13 @@ export const getAllEnergyGenerationRecordsBySolarUnitId = async (
           $sort: { "_id.date": -1 },
         },
       ]);
-      res.status(200).json(energyGenerationRecords);
+
+      if (!limit) {
+        res.status(200).json(energyGenerationRecords);
+        return;
+      }
+
+      res.status(200).json(energyGenerationRecords.slice(0, parseInt(limit)));
     }
   } catch (error) {
     next(error);
