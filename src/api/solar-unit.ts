dda9a98f6e2635025fc5347c3f9d190a3afc7c96
ManupaDev <manupadevsuite@gmail.com>
2025-11-23 22:07:04 +0530

    feat: add sync middleware for energy generation records and update solar unit route to include synchronization

--- src/api/solar-unit.ts
+++ src/api/solar-unit.ts
@@ -10,11 +10,12 @@ import {
 } from "../application/solar-unit";
 import { authenticationMiddleware } from "./middlewares/authentication-middleware";
 import { authorizationMiddleware } from "./middlewares/authorization-middleware";
+import { syncMiddleware } from "./middlewares/sync/sync-middleware";
 
 const solarUnitRouter = express.Router();
 
 solarUnitRouter.route("/").get(authenticationMiddleware, authorizationMiddleware, getAllSolarUnits).post(authenticationMiddleware, authorizationMiddleware, createSolarUnitValidator, createSolarUnit);
-solarUnitRouter.route("/me").get(authenticationMiddleware, getSolarUnitForUser);
+solarUnitRouter.route("/me").get(authenticationMiddleware, syncMiddleware, getSolarUnitForUser);
 solarUnitRouter
   .route("/:id")
   .get(authenticationMiddleware, authorizationMiddleware, getSolarUnitById)
